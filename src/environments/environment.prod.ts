e632f43a48218ce49e8c28aeb407259d40b85b69
Paresh Pawar <pareshpawar@Pareshs-MacBook-Pro.local>
2018-08-17 16:24:50 -0400

    oshop

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEkC7mHBxBIgSYCCWedxUflTiq-peu7i8",
+    authDomain: "oshop-2e6c6.firebaseapp.com",
+    databaseURL: "https://oshop-2e6c6.firebaseio.com",
+    projectId: "oshop-2e6c6",
+    storageBucket: "",
+    messagingSenderId: "431290362057"
+  }
+};
